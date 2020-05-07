8e92179b48602c36ede4f28be8106dd84480bff3
Marcus Lau <lau.marcus76@gmail.com>
2020-05-07 02:06:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+
+class CashRegister
+  
+  attr_accessor :total, :discount, :items, :price
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(item, price, quantity=1)
+    @total += (price*quantity)
+    @price = price
+    for i in 0..quantity-1
+      @items << item
+    end
+  end
+  
+  def apply_discount
+    if @discount > 0 
+      @total -= @discount*10
+      "After the discount, the total comes to $#{@total}."
+    else 
+      "There is no discount to apply."
+    end
+  end
+    
+  # gets last item added
+  # iterates thru items[] and removes all from array 
+  # for each removed item subtract that item price * item removed
+  def void_last_transaction
+    
+    
+  end
+    
+end
